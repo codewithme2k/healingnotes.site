02c10614204c2749aaf9bf3a5d573c94e2f444c3
codewithme2k <codewithme2k@gmail.com>
2025-04-26 16:04:39 +0700

    create header

---
+++ src/components/footer/footer-bottom.tsx
@@ -0,0 +1,31 @@
+import { clsx } from "clsx";
+
+import { Link } from "@/components/ui/link";
+import { SpotifyNowPlaying } from "@/components/ui/now-playing";
+import { SITE_METADATA } from "@/data/site-metadata";
+import MadeInVietNam from "@/icons/miv.svg";
+
+export function FooterBottom() {
+  return (
+    <div
+      className={clsx([
+        "pt-5 md:my-2",
+        "flex flex-col items-center justify-center gap-4 md:flex-row md:justify-between md:gap-16",
+        "border-t border-gray-200 dark:border-gray-700",
+      ])}
+    >
+      <SpotifyNowPlaying
+        className="w-full justify-center truncate [--artist-color:theme(colors.gray.500)] md:max-w-[50%] md:justify-start"
+        songEffect="underline"
+        showCover
+      />
+      <div className="flex items-center">
+        <Link href={SITE_METADATA.siteRepo}>
+          <span data-umami-event="made-in-vietnam">
+            <MadeInVietNam />
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
+}
